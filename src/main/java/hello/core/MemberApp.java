437842f99ba7a7738c7e23c6c303faada2a862f3
yeonzeop <ksp2617@naver.com>
2025-03-05 16:06:45 +0900

    Refactor: AppConfig (DI, OCP)

--- src/main/java/hello/core/MemberApp.java
+++ src/main/java/hello/core/MemberApp.java
@@ -7,8 +7,9 @@ import hello.core.member.MemberServiceImpl;
 
 public class MemberApp {
     public static void main(String[] args) {
-
-        MemberService memberService = new MemberServiceImpl();
+        AppConfig appConfig = new AppConfig();
+        MemberService memberService = appConfig.memberService();
+//        MemberService memberService = new MemberServiceImpl();
         Member member = new Member(1L,"memberA", Grade.VIP);
         memberService.join(member);
 
@@ -16,5 +17,7 @@ public class MemberApp {
         Member findMember=memberService.findMember(1L);
         System.out.println("new member = " + member.getName());
         System.out.println("find member = " + findMember.getName());
+
+
     }
 }
