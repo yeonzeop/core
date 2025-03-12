4c8d2d3464785b343d09dacd3b50b165b8bd9d2d
yeonzeop <ksp2617@naver.com>
2025-03-12 12:54:07 +0900

    Refactor: RateDiscountPolicy 수정

--- src/main/java/hello/core/discount/RateDiscountPolicy.java
+++ src/main/java/hello/core/discount/RateDiscountPolicy.java
@@ -11,6 +11,7 @@ public class RateDiscountPolicy implements DiscountPolicy {
     public int discount(Member member, int price) {
         if (member.getGrade() == Grade.VIP) {
             return (int) (price * ((double) discountPercent / 100)); // 흠.
+//            return price * discountPercent / 100;
         } else {
             return 0;
         }
