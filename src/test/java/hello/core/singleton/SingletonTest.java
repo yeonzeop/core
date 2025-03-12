bed0e9aa8dfd519ae553e5c879619f9aded834d5
yeonzeop <ksp2617@naver.com>
2025-03-12 13:25:04 +0900

    Feat: Singleton Pattern Test

--- src/test/java/hello/core/singleton/SingletonTest.java
+++ src/test/java/hello/core/singleton/SingletonTest.java
@@ -25,4 +25,13 @@ public class SingletonTest {
 
         Assertions.assertThat(memberService1).isNotSameAs(memberService2);
     }
+
+    @Test
+    @DisplayName("싱글톤 패턴 적용")
+    void singletonServiceTest(){
+        SingletonService singletonService1 = SingletonService.getInstance();
+        SingletonService singletonService2 = SingletonService.getInstance();
+
+        Assertions.assertThat(singletonService1).isSameAs(singletonService2);
+    }
 }
