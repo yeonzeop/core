d0d24076cd6a9c43a4e9b1ac22cc04c57b69031a
yeonzeop <ksp2617@naver.com>
2025-03-13 16:10:08 +0900

    Feat: Component Filter Test

---
+++ src/test/java/hello/core/scan/filter/ComponentFilterAppConfigTest.java
@@ -0,0 +1,30 @@
+package hello.core.scan.filter;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+public class ComponentFilterAppConfigTest {
+
+    @Test
+    void filterScan(){
+        AnnotationConfigApplicationContext ac =
+                new AnnotationConfigApplicationContext(ComponentFilterAppConfig.class);
+
+        BeanA beanA = ac.getBean(BeanA.class);
+
+        Assertions.assertThat(beanA).isInstanceOf(BeanA.class);
+    }
+
+    @Configuration
+    @ComponentScan(
+            includeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = MyIncludeComponent.class),
+            excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = MyExcludeComponent.class)
+    )
+    static class ComponentFilterAppConfig{
+
+    }
+}
