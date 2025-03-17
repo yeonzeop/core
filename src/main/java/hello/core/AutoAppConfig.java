8ccedf1e6b5c255c8fdc4bd96783f58a60228df5
yeonzeop <ksp2617@naver.com>
2025-03-17 10:54:12 +0900

    Feat: 수동 반 AutoAppConfig에 추가

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -1,5 +1,8 @@
 package hello.core;
 
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
@@ -10,5 +13,9 @@ import org.springframework.context.annotation.FilterType;
 )
 public class AutoAppConfig {
 
+//    @Bean(name = "memoryMemberRepository")
+//    MemberRepository memberRepository(){
+//        return new MemoryMemberRepository();
+//    }
 
 }
