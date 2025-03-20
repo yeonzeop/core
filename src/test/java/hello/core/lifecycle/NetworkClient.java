65e434ed136737f45509490ce1b392bc93ba3a0c
yeonzeop <ksp2617@naver.com>
2025-03-20 11:07:17 +0900

    Feat: initMethod, destroyMethod 사용

--- src/test/java/hello/core/lifecycle/NetworkClient.java
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,9 +1,6 @@
 package hello.core.lifecycle;
 
-import org.springframework.beans.factory.DisposableBean;
-import org.springframework.beans.factory.InitializingBean;
-
-public class NetworkClient implements InitializingBean, DisposableBean {
+public class NetworkClient {
 
     private String url;
 
@@ -17,27 +14,28 @@ public class NetworkClient implements InitializingBean, DisposableBean {
     }
 
     // 서비스 시작 시 호출
-    public void connect(){
+    public void connect() {
         System.out.println("connet : " + url);
     }
 
-    public void call(String message){
+    public void call(String message) {
         System.out.println("call: " + url + "   message = " + message);
     }
 
     // 서비스 종료 시 호출
-    public void disconnect(){
+    public void disconnect() {
         System.out.println("close : " + url);
     }
 
-    @Override
-    public void afterPropertiesSet() throws Exception {
+
+    public void init() throws Exception {
+        System.out.println("NetworkClient.init");
         connect();
         call("초기화 연결 메세지");
     }
 
-    @Override
-    public void destroy() throws Exception {
+    public void close() throws Exception {
+        System.out.println("NetworkClient.close");
         disconnect();
     }
 }
