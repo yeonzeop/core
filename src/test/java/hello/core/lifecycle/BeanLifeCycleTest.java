da31c30dc2a5917213fa9db65b22c62b78ef08d1
yeonzeop <ksp2617@naver.com>
2025-03-20 11:09:36 +0900

    Feat: PostConstruct, PreDestroy 애노테이션 사용

--- src/test/java/hello/core/lifecycle/BeanLifeCycleTest.java
+++ src/test/java/hello/core/lifecycle/BeanLifeCycleTest.java
@@ -16,7 +16,7 @@ public class BeanLifeCycleTest {
 
     @Configuration
     static class LifeCycleConfig{
-        @Bean(initMethod = "init", destroyMethod = "close")
+        @Bean
         public NetworkClient networkClient(){
             NetworkClient networkClient = new NetworkClient();
             networkClient.setUrl("http://hello-spring.dev"); // 무엇이 의존관계인가
